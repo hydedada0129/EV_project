d4d96ce4de35e3aa692cbde19f287e893091753b
oem <hydedada0129@gmail.com>
2025-05-19 20:30:26 +0800

    done of crontab setup and work properly. next: pull mysql data within the past 24 hours for excel reports and uploading.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 import os
 import dropbox
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import mysql.connector
+import datetime
 from openpyxl import load_workbook
 from datetime import datetime
 from dropbox.files import WriteMode
-
-from secrets_loader import get_secret
+# from secrets_loader import get_secret
 from upload_to_db import upload_file_to_dropbox
+# from crontab import CronTab
 
 #TO DO: requirements.txt
 #save your excel template in project root folder
@@ -31,6 +32,7 @@ def fetch_latest_submission(submission_id=None):
     conn = mysql.connector.connect(**DB_CONFIG)
     cursor = conn.cursor(dictionary=True)
     sql = "SELECT id, user_id, field_1, field_2, created_at FROM wp_submissions"
+    
     params = ()
     if submission_id:
         sql += " WHERE id = %s"
@@ -66,6 +68,9 @@ def main():
 
         #upload to Dropbox
         dest = upload_file_to_dropbox(OUTPUT_PATH)
+
+        print(f"output path: {OUTPUT_PATH}")
+
         print(f'Excel report has been uploaded: {dest}')
 
         # 清理本地檔案 (not decide yet, comment it first)
@@ -76,6 +81,25 @@ def main():
         print(f"執行失敗：{e}")
         exit(1)     #1 : force terminates the whole Python script, and return a non-zero code to Operation System.
 
+# def schedule_task():
+#     # Initialize CronTab for current user in Docker container
+#     cron = CronTab(user=True)
+#     # Remove existing cron jobs to avoid duplicates
+#     cron.remove_all()
+#     # Create new cron job to run 'python /app/main.py --run' every minute
+#     job = cron.new(command='/home/oem/wordpress-docker/.venv/bin/python /home/oem/wordpress-docker/main.py --run')
+#     # Set schedule: every minute
+#     job.setall('* * * * *')
+#     # Write cron job to system
+#     cron.write()
+#     print("Cron job scheduled: Every minute")
 
 if __name__== "__main__":
     main()
+
+# if __name__ == '__main__':
+#     import sys
+#     if len(sys.argv) > 1 and sys.argv[1] == '--run':
+#         process_submissions()
+#     else:
+#         schedule_task()
